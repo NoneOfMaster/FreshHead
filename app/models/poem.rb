74b8a259b4d8f777fe5b7b3bcaf614b1b1969c37
NoneOfMaster <rcraigt@gmail.com>
2016-05-21 14:08:24 -0700

    PoetryDB.org backs up to Poems.com not Poetry.net
    - Poetry.net's API turned out also to be horrible so now the backup for PoetryDB.org is a nokogiri scrape of the daily poem from Poems.com

--- app/models/poem.rb
+++ app/models/poem.rb
@@ -1,30 +1,41 @@
 require 'open-uri'
+require 'nokogiri'
 
 class Poem < ActiveRecord::Base
 
   belongs_to :batch
 
   before_save do |p|
-    p.get_from_poetry_db
-    # can randomize between both dbs, and also default to poetry.net if poetrydb fails
-    # p.get_from_poetry_dot_net
-  end
-
-  def get_from_poetry_dot_net
-
+    # to make sure this poem isn't already in the db - be sure to test
+    while self.poem == nil || Poem.where(:poem => self.poem).count > 0 
+      p.get_from_poetry_db
+    end
   end
 
   def get_from_poetry_db
-    # to make sure this poem isn't already in the db - add to test
-    while self.poem == nil || Poem.where(:poem => self.poem).count > 0  
-      line_count = rand(3..11)
-      full_poems_obj = JSON.parse(open("http://poetrydb.org/linecount/#{line_count}:abs/all").read)
-      ## write something here to abort a failure and switch to poetry.net if poetrydb is not responding
-      single_poem_obj = full_poems_obj.sample
+    line_count = rand(3..11)
+    http = "http://poetrydb.org/linecount/#{line_count}:abs/all"
+    begin
+      ## any error or lag of 15 seconds falls back to poems.com
+      @full_poems_obj = JSON.parse(open(http, {:read_timeout => 15}).read) 
+    rescue
+      get_from_poems_dot_com
+    else
+      puts "from Poetrydb.org"
+      single_poem_obj = @full_poems_obj.sample
       self.title = single_poem_obj["title"]
       self.poet = single_poem_obj["author"]
       self.poem = single_poem_obj["lines"].join("<br>")
     end
+  end
+
+  def get_from_poems_dot_com
+    puts "from Poems.com"
+    html = open("http://poems.com/today.php")
+    poem_obj = Nokogiri::HTML(html)
+    self.title = poem_obj.css("#page_title").text
+    self.poet = poem_obj.css("#byline").text
+    self.poem = poem_obj.css("#poem").text.gsub(/\n/, "<br>")
   end
 
 end
